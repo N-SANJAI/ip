864b6107068401a78ab9864edc1a5e98dad72c7e
Sanjai <sanjaitechtips@gmail.com>
2026-01-28 23:42:46 +0800

    feat: implement Level 0 (Rename, Greet, Exit)

---
+++ src/main/java/Heisenberg.java
@@ -0,0 +1,20 @@
+public class Heisenberg {
+    public static void main(String[] args) {
+        String line = "____________________________________________________________";
+        String logo = " _   _       _                  _                          \n"
+                + "| | | | ___ (_) ___  ___ _ __  | |__   ___ _ __ __ _ \n"
+                + "| |_| |/ _ \\| |/ __|/ _ \\ '_ \\ | '_ \\ / _ \\ '__/ _` |\n"
+                + "|  _  |  __/| |\\__ \\  __/ | | || |_) |  __/ | | (_| |\n"
+                + "|_| |_|\\___||_| ___/\\___|_| |_||_.__/ \\___|_|  \\__, |\n"
+                + "                                               |___/ \n";
+
+        System.out.println(line);
+        System.out.println("Say My Name \n" + logo);
+        System.out.println("You're god dam right");
+        System.out.println(line);
+
+        System.out.println("You don't need a regular chatbot. You need a *criminal* chatbot.");
+        System.out.println("S'all good, man! See you in court.");
+        System.out.println(line);
+    }
+}
